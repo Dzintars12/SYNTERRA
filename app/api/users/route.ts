6de0813f4321160f41e66735e41ae451cb8f73fc
Dzintars <dzintars.ansons@gmail.com>
2026-05-21 22:56:38 +0200

    Add users API route foundation

---
+++ app/api/users/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import { listUsers } from "../../../src/server/userRepository";
+
+export async function GET() {
+  const users = await listUsers();
+
+  return NextResponse.json({
+    status: "success",
+    count: users.length,
+    users,
+  });
+}
